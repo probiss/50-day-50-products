68675831c5c7391928745df11e85f190c3fe9201
probiss <bulut.cihad@gmail.com>
2023-06-12 23:34:14 +0300

    vote color changed.

--- 17/17.js
+++ 17/17.js
@@ -30,7 +30,7 @@ function showMovies(movies) {
             <img src="${IMG_PATH + poster_path}" alt="">
             <div class="movie-info">
                 <h3>${title}</h3>
-                <span class="{getClassByRate(vote_average)}" >${vote_average}</span>
+                <span class="${getClassByRate(vote_average)}" >${vote_average}</span>
             </div>
             <div class="overview">
                 <h3>Overview</h3>
